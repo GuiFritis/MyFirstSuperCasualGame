6dc6f66764840909ecf2580732babc4138b1e3cb
GuiFritis <Guilhermecfritis@gmail.com>
2022-01-02 19:09:44 -0300

    add cinemachine fix player walk

--- GuilhermeFritis-MobileSuperCasual/Assets/Script/Player/PlayerController.cs
+++ GuilhermeFritis-MobileSuperCasual/Assets/Script/Player/PlayerController.cs
@@ -5,15 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+
     public float speed = 1f;
+
+    public string enemyTag = "Enemy";
+
+    private bool _canRun = true;
+    private Vector3 _pos;
+
+    void Start()
+    {
+        _canRun = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        WalkForward();
+        if(_canRun){
+            Move();
+            WalkForward();
+        }
+    }
+
+    private void Move(){        
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);    
     }
 
     private void WalkForward(){
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag(enemyTag)){
+            _canRun = false;
+        }
     }
 }
